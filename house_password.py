cdda397ff9cab1fc5c402355b59259ffe83edd6f
BrendanArthurRing <brendanarthurring@gmail.com>
2020-03-12 07:31:45 +0900

    finished this kata

--- house_password.py
+++ house_password.py
@@ -1,18 +1,40 @@
 def checkio(data: str) -> bool:
-
-    # replace this for solution
-    return True or False
-
+	
+	conditions = []
+	
+	if len(data) >= 10:
+		conditions.append(True)
+	else:
+		conditions.append(False)
+	
+	if any(char.isdigit() for char in data):
+		conditions.append(True)
+	else:
+		conditions.append(False)
+	
+	if any(char.isupper() for char in data):
+		conditions.append(True)
+	else:
+		conditions.append(False)
+	
+	print(conditions)
+	
+	if all(conditions):
+		return True
+	else:
+		return False
+	
 # Some hints
 # Just check all conditions
 
 
 if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio('A1213pokl') == False, "1st example"
-    assert checkio('bAse730onE4') == True, "2nd example"
-    assert checkio('asasasasasasasaas') == False, "3rd example"
-    assert checkio('QWERTYqwerty') == False, "4th example"
-    assert checkio('123456123456') == False, "5th example"
-    assert checkio('QwErTy911poqqqq') == True, "6th example"
-    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+	# These "asserts" using only for self-checking and not necessary for auto-testing
+	assert checkio('A1213pokl') == False, "1st example"
+	assert checkio('bAse730onE4') == True, "2nd example"
+	assert checkio('asasasasasasasaas') == False, "3rd example"
+	assert checkio('QWERTYqwerty') == False, "4th example"
+	assert checkio('123456123456') == False, "5th example"
+	assert checkio('QwErTy911poqqqq') == True, "6th example"
+	print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+
