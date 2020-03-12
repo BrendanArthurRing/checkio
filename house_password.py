4f80238a750f288e277b46b0f7e141d955f926a1
BrendanArthurRing <brendanarthurring@gmail.com>
2020-03-12 11:31:43 +0900

    removed print statement debug not needed, this has passed

--- house_password.py
+++ house_password.py
@@ -1,24 +1,24 @@
 def checkio(data: str) -> bool:
 
-	conditions = []
+    conditions = []
 
     # 10 or more characters
-	if len(data) >= 10:
-		conditions.append(True)
-	else:
-		conditions.append(False)
+    if len(data) >= 10:
+        conditions.append(True)
+    else:
+        conditions.append(False)
 
     # Contains digit
-	if any(char.isdigit() for char in data):
-		conditions.append(True)
-	else:
-		conditions.append(False)
+    if any(char.isdigit() for char in data):
+        conditions.append(True)
+    else:
+        conditions.append(False)
 
     # Contains Upper
-	if any(char.isupper() for char in data):
-		conditions.append(True)
-	else:
-		conditions.append(False)
+    if any(char.isupper() for char in data):
+        conditions.append(True)
+    else:
+        conditions.append(False)
 
     # Contains Lower
     if any(char.islower() for char in data):
@@ -26,25 +26,22 @@ def checkio(data: str) -> bool:
     else:
         conditions.append(False)
 
-	print(conditions)
-	
-	if all(conditions):
-		return True
-	else:
-		return False
-	
+    if all(conditions):
+        return True
+    else:
+        return False
+
 # Some hints
 # Just check all conditions
 
 
 if __name__ == '__main__':
-	# These "asserts" using only for self-checking and not necessary for auto-testing
-	assert checkio('A1213pokl') == False, "1st example"
-	assert checkio('bAse730onE4') == True, "2nd example"
-	assert checkio('asasasasasasasaas') == False, "3rd example"
-	assert checkio('QWERTYqwerty') == False, "4th example"
-	assert checkio('123456123456') == False, "5th example"
-	assert checkio('QwErTy911poqqqq') == True, "6th example"
-    assert checio('DHJK87DSKJHWW68D') == False, "7th example"
-	print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
-
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    assert checkio('DHJK87DSKJHWW68D') == False, "7th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
