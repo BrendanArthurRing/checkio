adb085628e09c9afea5f7a88abbe09be28b2d233
Brendan Arthur Ring <BrendanArthurRing@gmail.com>
2020-04-12 14:01:21 +0900

    complete

---
+++ bigger_price.py
@@ -0,0 +1,44 @@
+# https://py.checkio.org/en/mission/bigger-price/
+
+def bigger_price(limit: int, data: list) -> list:
+    """
+        TOP most expensive goods
+    """
+    # your code here
+    from operator import itemgetter
+    data.sort(key=itemgetter('price'), reverse=True)
+    return data[:limit]
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    print('Example:')
+    pprint(bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]))
+
+    # These "asserts" using for self-checking and not for auto-testing
+    assert bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]) == [
+        {"name": "wine", "price": 138},
+        {"name": "bread", "price": 100}
+    ], "First"
+
+    assert bigger_price(1, [
+        {"name": "pen", "price": 5},
+        {"name": "whiteboard", "price": 170}
+    ]) == [{"name": "whiteboard", "price": 170}], "Second"
+
+    print('Done! Looks like it is fine. Go and check it')
+
+
+# Research
+# Sorting a list of dictionaries
+# https://www.youtube.com/watch?v=YNWjwXjyUts
