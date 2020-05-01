d8952a4dad5d8c2e0976e7c83c1939c7125b4d39
BrendanArthurRing <brendanarthurring@gmail.com>
2020-05-02 02:18:10 +0900

    inital comit

---
+++ morse_decoder.py
@@ -0,0 +1,44 @@
+# https://py.checkio.org/en/mission/morse-decoder/
+
+import re
+
+MORSE = {'.-':    'a', '-...':  'b', '-.-.':  'c',
+         '-..':   'd', '.':     'e', '..-.':  'f',
+         '--.':   'g', '....':  'h', '..':    'i',
+         '.---':  'j', '-.-':   'k', '.-..':  'l',
+         '--':    'm', '-.':    'n', '---':   'o',
+         '.--.':  'p', '--.-':  'q', '.-.':   'r',
+         '...':   's', '-':     't', '..-':   'u',
+         '...-':  'v', '.--':   'w', '-..-':  'x',
+         '-.--':  'y', '--..':  'z', '-----': '0',
+         '.----': '1', '..---': '2', '...--': '3',
+         '....-': '4', '.....': '5', '-....': '6',
+         '--...': '7', '---..': '8', '----.': '9'
+        }
+
+def morse_decoder(code):
+    ml = []
+    c = code.split(" ")
+    for i in c:
+        try:
+            ml.append(MORSE[i])
+        except:
+            ml.append(i)
+    message = re.sub(' +', ' ', ''.join(char or ' ' for char in ml).capitalize())
+    return message
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(morse_decoder('... --- ...'))
+
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
+    assert morse_decoder("..--- ----- .---- ---..") == "2018"
+    assert morse_decoder(".. -   .-- .- ...   .-   --. --- --- -..   -.. .- -.--") == "It was a good day"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+
+# Research
+# https://stackoverflow.com/questions/1546226/what-is-a-simple-way-to-remove-multiple-spaces-in-a-string
