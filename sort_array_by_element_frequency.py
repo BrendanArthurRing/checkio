160831f2898e417f9e47898c10a213f234fa7a6f
BrendanArthurRing <brendanarthurring@gmail.com>
2020-05-02 01:29:18 +0900

    testing

--- sort_array_by_element_frequency.py
+++ sort_array_by_element_frequency.py
@@ -1,8 +1,21 @@
 # https://py.checkio.org/en/mission/sort-array-by-element-frequency/
 
+from collections import Counter
+
 def frequency_sort(items):
-    # your code here
-    return None
+    return [item for items, c in Counter(items).most_common() for item in [items] * c]
+    # I dont understand how this works, but it works.
+    # I know about list comprehensions, but not sure why there is a "'"
+    # and no idea how [items] * c actually works.
+    # I found this code here:
+    # https://www.geeksforgeeks.org/python-sort-list-elements-by-frequency/
+    # It would be nice if they actually explained how it works.
+    # Bummed that I couldn't figure this out all on my own.
+    # the "If two elements have the same frequency, they should end up in 
+    # the same order as the first appearance in the iterable." part
+    # really stumped me.
+
+# (values) = [ (exression) for (value) in (collection) ] 
 
 
 if __name__ == '__main__':
